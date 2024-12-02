d424fbc5d1e365ae7f3cc36fd3f3698ea6bb4d43
teenSpirit <kll78460@gmail.com>
2024-12-02 20:51:13 +0800

    fix hyperlink and login text

--- app/views/components/loginBar.php
+++ app/views/components/loginBar.php
@@ -4,23 +4,22 @@
                 <div class="col-12">
                     <div class="main_header d-flex justify-content-between align-items-center">
                         <div class="header_logo">
-                            <a class="sticky_none" href="main.html"><img src="assets/img/logo/logo.png" alt="Logo"></a>
+                            <a class="sticky_none" href="?url=home"><img src="assets/img/logo/logo.png" alt="Logo"></a>
                         </div>
                         <div class="main_menu d-none d-lg-block">
                             <nav>
                                 <ul class="d-flex">
-                                    <li><a href="main.html">Home</a></li>
-                                    <li><a href="shop1.html">Shop</a></li>
+                                    <li><a href="?url=home">Home</a></li>
+                                    <li><a href="?url=show/merchandises">Shop</a></li>
                                 </ul>
                             </nav>
                         </div>
                         <div class="header__ridebar d-flex align-items-center">
                             <div class="header_account">
                                 <ul class="d-flex">
-                                    <li class="shopping_cart"><a href="#"><img src="assets/img/icon/cart.png" alt="Cart"></a></li>
                                     <li class="account_link_menu"><a href="#"><img src="assets/img/icon/person.png" alt="Account"></a>
                                         <ul class="dropdown_account_link">
-                                            <li><a href="LogIn.html">Log in</a></li>
+                                            <li><a href="?url=loginPage">Log in</a></li>
                                         </ul>
                                     </li>
                                 </ul>
