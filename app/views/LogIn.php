77ac218f856c598bb803aa01575129fe32ec3083
teenSpirit <kll78460@gmail.com>
2024-12-03 16:06:34 +0800

    fix login failure

--- app/views/LogIn.php
+++ app/views/LogIn.php
@@ -1,6 +1,6 @@
 <?php require APP_ROOT . 'views/components/loginBar.php'; ?>
 <?php require APP_ROOT . 'views/include/LoginHeader.php'; ?>
-
+<!--
 <body>
     <div class="login-container">
         <h2>Login</h2>
@@ -23,6 +23,36 @@
             <button type="submit" >Login</button>
         </form>
     </div>
+</body>
+
+</html>
+-->
+
+<body>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form id="loginForm" method="post">
+            <label for="username">Username</label>
+            <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+                required />
+
+            <label for="password">Password</label>
+            <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                required />
+
+            <button type="submit" >Login</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="assets/js/login.js"></script>
 </body>
 
 </html>
